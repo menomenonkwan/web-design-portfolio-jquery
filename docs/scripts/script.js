56304b960f51b994f45776e6394d6cd9f3bb1d80
menomenonkwan <menomenonkwan@gmail.com>
2022-04-02 17:11:40 -0700

    mobile menu work

--- docs/scripts/script.js
+++ docs/scripts/script.js
@@ -1,9 +1,18 @@
 $(document).ready(function() {
+
+  // Window resize
+  $(window).on('resize', function(){
+    const screenHeight = $(this);
+
+    if (screenHeight.width() >= 768) { 
+      $('nav').show();
+    }
+  });
+
   // Cursor
   $(document).on("mousemove", (e) => {
     const cursor = $(".cursor");
     const top = e.pageY - $(window).scrollTop();
-
     cursor.attr(
       "style",
       `top: ${top -10}px; left: ${e.pageX - 10}px;`
@@ -17,19 +26,35 @@ $(document).ready(function() {
     parallax.css('transform', 'translateY(' + scrollPosition * 0.5 + 'px' + ')');
   });
 
+  // Hamburger Menu
+  $('.hamburger').click(() => {
+    $('.hamburger').toggleClass('open');
+    $('nav').slideToggle();
+  });
+
+  $('nav a').click(() => {
+    const screenWidth = $(this).width();
+
+    if(screenWidth < 768) {
+      $('nav').slideToggle();
+      $('.hamburger').toggleClass('open');
+    }
+  })
+
   // Desktop menu
   $(window).on('scroll', () => {
     const scrollPosition = $(this).scrollTop();
+    const screenWidth = $(this).width();
     const header = $('header');
     const brand = $('header h3');
     const nav = $('nav');
-
-    if (scrollPosition > 210) {
-      header.addClass('menu-split');
+    
+    if (scrollPosition > 210 && screenWidth >= 768) {
+      header.addClass('menu-fade');
       brand.addClass('menu-shrink');
       nav.addClass('menu-shrink');
     } else {
-      header.removeClass('menu-split');
+      header.removeClass('menu-fade');
       brand.removeClass('menu-shrink');
       nav.removeClass('menu-shrink');
     }
